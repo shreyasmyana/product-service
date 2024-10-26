838912149386e2140e33d9613b68377773bd4618
Shreyas Myana <shreyasmyana1997@gmail.com>
2024-10-26 12:47:32 +0530

    feat: add search endpoint

--- products/views.py
+++ products/views.py
@@ -1,8 +1,10 @@
-from django.shortcuts import render
 from django.http import HttpResponse
 from rest_framework import viewsets
 from .models import Category, Product
 from .serializers import CategorySerializer, ProductSerializer
+from rest_framework.decorators import action
+from rest_framework.response import Response
+from django.db.models import Q
 
 # Create your views here.
 def servicehealth(request):
@@ -15,3 +17,12 @@ class CategoryViewSet(viewsets.ModelViewSet):
 class ProductViewSet(viewsets.ModelViewSet):
     queryset = Product.objects.all()
     serializer_class = ProductSerializer
+
+    @action(detail=False, methods=['get'])
+    def search(self, request):
+        name_query = request.query_params.get('name', None)
+        if name_query:
+            products = Product.objects.filter(name__icontains=name_query)
+            serializer = self.get_serializer(products, many=True)
+            return Response(serializer.data)
+        return Response({"error": "Please provide a title to search for."}, status=400)
